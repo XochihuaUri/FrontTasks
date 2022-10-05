a196c0adba8abd48a94b8483a67c2fbd4c00fed6
Uriel Xochihua <Xochihua.Sotarriba@jala.university>
2022-10-04 20:24:59 -0500

    adding task 6

---
+++ Task6/task6.js
@@ -0,0 +1,34 @@
+// function PrintName(name){
+//     setTimeout(function(){
+//         console.log("The name received is: " + name);
+//     },
+//     5000
+//     );
+// }
+
+// PrintName("ASD");
+
+function job(data) {
+    return new Promise((resolved, rejected) => {
+        if(isNaN(data)){
+        rejected('error');
+        } else if(data%2 !== 0) {
+        setTimeout(resolved, 1000, 'odd');
+        } else {
+        setTimeout(rejected, 2000, 'even');
+        }
+        });
+}
+
+job("A")
+.then(res => console.log(res))
+.catch(e=> console.log(e))
+
+job(1)
+.then(res => console.log(res))
+.catch(e=> console.log(e))
+
+job(2)
+.then(res => console.log(res))
+.catch(e=> console.log(e))
+
